56cfad84a49cef6c00a4887d3749a564bef7eece
Mohamadkh01 <mohamadkh6324@gmail.com>
2025-04-19 09:13:12 +0300

    create a function to get User choice

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 let compChoice;
+let humanChoice;
 
+const Rock = 0;
+const Paper = 1;
+const Scissors = 2;
 
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3);
     switch (x){
         case 0:
-            compChoice = "Rock";
+            compChoice = Rock;
             break;
         case 1:
-            compChoice = "Paper";
+            compChoice = Paper;
             break;
         case 2:
-            compChoice = "Scissors";
+            compChoice = Scissors;
             break;
-        default:
-            compChoice = "Error";
     }
 
     return compChoice;
+}
+
+function getHumanChoice(){
+    let str = prompt("Rock? Paper? Scissors?");
+    str = str.toLowerCase();
+
+    switch(str){
+        case "rock":
+            humanChoice = Rock;
+            break;
+        case "paper":
+            humanChoice = Paper;
+            break;
+        case "scissors":
+            humanChoice = Scissors;
+            break;
+        default:
+            humanChoice = Rock;
+            break;
+    }
+
 }

c74a6f22b2c37e5592cb77dcdb0c05a40d4c4313
Mohamadkh01 <mohamadkh6324@gmail.com>
2025-04-19 09:39:42 +0300

    create a function to play a round

--- script.js
+++ script.js
@@ -47,3 +47,44 @@ function getHumanChoice(){
     return humanChoice;
 }
 
+function playRound(humanChoice, compChoice){
+    if(compChoice == Rock){
+        if(humanChoice == Rock){
+            console.log("it's a tie!! play again!");
+        }
+        else if(humanChoice == Paper){
+            console.log("You win! Paper beats rock!");
+            humanScore++;
+        }
+        else if(humanChoice == Scissors){
+            console.log("You lose! Rock beats Scissors!");
+            compScore++;
+        }
+    }
+    else if(compChoice == Paper){
+        if(humanChoice == Rock){
+            console.log("You lose! Paper beats Rock!");
+            compScore++;
+        }
+        else if(humanChoice == Paper){
+            console.log("it's a tie!! play again!");
+        }
+        else if(humanChoice == Scissors){
+            console.log("You win! scissors beat Paper!");
+            humanScore++;
+        }
+    }
+    else if(compChoice == Scissors){
+        if(humanChoice == Rock){
+            console.log("You win! Rock beats Scissors!");
+            humanScore++;
+        }
+        else if(humanChoice == Paper){
+            console.log("You lose! Scissors beat Paper!");
+            compScore++;
+        }
+        else if(humanChoice == Scissors){
+            console.log("It's a tie!! play again!");
+        }
+    }
+}
